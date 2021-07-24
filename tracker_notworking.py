58823e247cdec6b5b207c9ed0cac9c699c063291
ccf-2012 <87898914+ccf-2012@users.noreply.github.com>
2021-07-24 19:51:32 +0800

    Create tracker_notworking.py

---
+++ tracker_notworking.py
@@ -0,0 +1,26 @@
+import qbittorrentapi
+import urllib.parse
+
+qbt_client = qbittorrentapi.Client(host='192.168.1.8', port=8189, username='admin', password='adminadmin')
+
+try:
+    qbt_client.auth_log_in()
+except qbittorrentapi.LoginFailed as e:
+    print(e)
+
+# display qBittorrent info
+print(f'qBittorrent: {qbt_client.app.version}')
+print(f'qBittorrent Web API: {qbt_client.app.web_api_version}')
+
+count = 0
+for torrent in qbt_client.torrents_info(sort='name'):
+    # 这里我偷懒了，用者自便
+    tr3 = torrent.trackers[3]
+
+    # 列出tracker 未工作的 stalledUP
+    if tr3['status'] == 4:
+        count += 1
+        print(f'{torrent.hash[-6:]}: \033[32m{torrent.name}\033[0m ({torrent.state})')
+        print( f'\033[31m {urllib.parse.urlparse(tr3["url"]).netloc}\033[0m   \033[34m  {tr3["msg"]} \033[0m')
+
+print(f'Total: {count}')
