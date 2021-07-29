c6f217c9a5dc0443bd2f907489b3ef33f470984d
geng <fuse66@163.com>
2021-07-29 15:38:57 +0800

    seperate Movie4K,MovieWeb4K,MovieBDMV4K

--- autocategory.py
+++ autocategory.py
@@ -13,27 +13,29 @@ QB_PARAM = {
 # 注意qb的存储位置
 QB_ROOT = '/Downloads/'
 # 分了这样 8 类，后面数字是输出字符的颜色，真改之前方便检查
-CATEGORIES = [
-    ['TV', '32'],
-    ['MV', '31'],
-    ['Audio', '34'],
-    ['MovieEncode', '36'],
-    ['MovieRemux', '36'],
-    ['MovieWebdl', '36'],
-    ['MovieBDMV', '35'],
-    ['Other', '33']
-]
-counts = [0, 0, 0, 0, 0, 0, 0, 0]
+CATEGORIES = {
+    'TV': ['TV', '32', 0],
+    'MV': ['MV', '31', 0],
+    'Audio': ['Audio', '34', 0],
+    'MovieEncode': ['MovieEncode', '36', 0],
+    'MovieRemux': ['MovieRemux', '36', 0],
+    'Movie4K': ['Movie4K', '36', 0],
+    'MovieWebdl': ['MovieWebdl', '36', 0],
+    'MovieWeb4K': ['MovieWeb4K', '36', 0],
+    'MovieBDMV': ['MovieBDMV', '35', 0],
+    'MovieBDMV4K': ['MovieBDMV4K', '35', 0],
+    'Other': ['Other', '33', 0]
+}
 
 
-def setCategory(torrent, catid):
-    counts[catid] += 1
+def setCategory(torrent, catstr):
+    CATEGORIES[catstr][2] += 1
     print(
-        f'{CATEGORIES[catid][0]}: \033[{CATEGORIES[catid][1]}m{torrent.name}\033[0m ({torrent.state})')
+        f'{CATEGORIES[catstr][0]}: \033[{CATEGORIES[catstr][1]}m{torrent.name}\033[0m ({torrent.state})')
     # 真要修改，就去掉下面的注释
-    # torrent.setCategory(CATEGORIES[catid][0])
+    # torrent.setCategory(CATEGORIES[catstr][0])
     # 要一并修改磁盘上存储的位置就把下面注释拿掉，一定抬头看一下上面的QB_ROOT的位置
-    # torrnt.setLocation(QB_ROOT+CATEGORIES[catid][0])
+    # torrent.setLocation(QB_ROOT+CATEGORIES[catstr][0])
     return
 
 
@@ -43,7 +45,7 @@ def qbAutoCategory(qbt_client):
     # 有些组生产 TV Series，但是在种子名上不显示 S01 这些
     tvGroups = ['CMCTV',  'FLTTH']
     # 有些Web组，即生产TV又生产Movie，种子名上又不显示，得看文件
-    webGroups = ['CHDWEB', 'PTerWEB', 'HaresWEB', 'DBTV', 'QHStudio', 
+    webGroups = ['CHDWEB', 'PTerWEB', 'HaresWEB', 'DBTV', 'QHStudio',
                  'LeagueWEB', 'HDCTV', '52KHD', 'PTHweb', 'OurTV', 'iLoveTV']
     # 有些组专门生产 MV
     mvGroups = ['PterMV']
@@ -61,41 +63,54 @@ def qbAutoCategory(qbt_client):
             # 有season和episode 或 专作TV的Group
             if info.__contains__('season') or info.__contains__('episode') or \
                     (info.__contains__('encoder') and info['encoder'] in tvGroups):
-                setCategory(torrent, 0)
+                setCategory(torrent, 'TV')
             elif re.search(r'S0\d\W|\d季|第\w{1,3}季\W', torrent.name, re.I):
-                setCategory(torrent, 0)
+                setCategory(torrent, 'TV')
             elif re.search(r'\Wcomplete\W|全\d+集|\d+集全', torrent.name, re.I):
-                setCategory(torrent, 0)
+                setCategory(torrent, 'TV')
             elif info.__contains__('encoder') and info['encoder'] in webGroups:
+                ### TODO: 取出里面的文件名进行parse检查
                 if len(torrent.files) > 4:
-                    setCategory(torrent, 0)
+                    setCategory(torrent, 'TV')
             elif info.__contains__('encoder') and info['encoder'] in mvGroups:
-                setCategory(torrent, 1)
+                setCategory(torrent, 'MV')
             elif info.__contains__('encoder') and info['encoder'] in audioGroups:
-                setCategory(torrent, 2)
+                setCategory(torrent, 'Audio')
             elif info.__contains__('quality'):
                 # 来源为原盘的
                 if info['quality'] in ['Blu-ray']:
                     # Remux, 压制 还是 原盘
                     if re.search(r'\WREMUX\W', torrent.name, re.I):
-                        setCategory(torrent, 4)
+                        if info.__contains__('resolution') and info['resolution'] == '2160p':
+                            setCategory(torrent, 'Movie4K')
+                        else:
+                            setCategory(torrent, 'MovieRemux')
                     elif re.search(r'\Wx265\W|\Wx264\W', torrent.name, re.I):
-                        setCategory(torrent, 3)
+                        if info.__contains__('resolution') and info['resolution'] == '2160p':
+                            setCategory(torrent, 'Movie4K')
+                        else:
+                            setCategory(torrent, 'MovieEncode')
                     else:
-                        setCategory(torrent, 6)
+                        if info.__contains__('resolution') and info['resolution'] == '2160p':
+                            setCategory(torrent, 'MovieBDMV4K')
+                        else:
+                            setCategory(torrent, 'MovieBDMV')
                 # 来源是 WEB-DL
                 elif info['quality'] in ['WEB-DL']:
-                    setCategory(torrent, 5)
+                    if info.__contains__('resolution') and info['resolution'] == '2160p':
+                        setCategory(torrent, 'MovieWeb4K')
+                    else:
+                        setCategory(torrent, 'MovieWebdl')
             elif info.__contains__('encoder') and info['encoder'].find(movieEncodeGroup):
-                setCategory(torrent, 3)
+                setCategory(torrent, 'MovieEncode')
             else:
-                setCategory(torrent, 7)
+                setCategory(torrent, 'Other')
 
-    for i in range(len(counts)):
-        print(f'{CATEGORIES[i][0]} : {counts[i]}')
-    print(f'Total : {sum(counts)}')
-
-###
+    mvSum = 0
+    for cat in CATEGORIES.keys():
+        print(f'{CATEGORIES[cat][0]} : {CATEGORIES[cat][2]}')
+        mvSum += CATEGORIES[cat][2]
+    print(f'Total : {mvSum}')
 
 
 def main():
