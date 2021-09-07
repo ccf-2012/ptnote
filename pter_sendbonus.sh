44cbec05620f6187b7b9a437a10d187de9da079e
geng <fuse66@163.com>
2021-09-07 22:42:32 +0800

    送猫粮

--- pter_sendbonus.sh
+++ pter_sendbonus.sh
@@ -5,12 +5,12 @@ for i in {1..5}
 do
     # ...用户名...替换为你要送的用户username 
     # ...COOKIE...替换为你猫站上的Cookie
-	cm=$(curl -d 'username=...用户名...&bonusgift=10000&message=enjoy&option=13' -H 'Cookie:...COOKIE...'   "https://pterclub.com/mybonus.php?action=exchange")
-	ret=$(echo $cm)
-	result=$(echo $ret | grep "你成功赠送了猫粮。")
-	if [[ "$result" != "" ]];then
+    cm=$(curl -d 'username=...用户名...&bonusgift=10000&message=enjoy&option=13' -H 'Cookie:...COOKIE...'   "https://pterclub.com/mybonus.php?action=exchange")
+    ret=$(echo $cm)
+    result=$(echo $ret | grep "你成功赠送了猫粮。")
+    if [[ "$result" != "" ]];then
         echo "赠送猫粮 失败"
-	else
+    else
         echo "赠送猫粮 成功"
-	fi
+    fi
 done
