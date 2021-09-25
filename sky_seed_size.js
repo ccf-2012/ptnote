6639a173a5ccee19c478dd538188c75f195d2fc9
geng <fuse66@163.com>
2021-09-25 22:58:23 +0800

    update sky

--- sky_seed_size.js
+++ sky_seed_size.js
@@ -11,76 +11,99 @@
 // ==/UserScript==
 
 function formatBytes(bytes, decimals = 2) {
-    if (bytes === 0) return "0 Bytes";
-  
-    const k = 1024;
-    const dm = decimals < 0 ? 0 : decimals;
-    const sizes = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
-  
-    const i = Math.floor(Math.log(bytes) / Math.log(k));
-  
-    return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + " " + sizes[i];
-  }
-  
-  function getSeedList(seedHtml) {
-    var seedList = seedHtml.querySelectorAll("table > tbody > tr > td:nth-child(2) > a > b");
-    var seedListSize = seedHtml.querySelectorAll("table > tbody > tr > td:nth-child(6)");
-    var summary = GM_addElement(
-      document.querySelector("#outer > table > tbody > tr > td > table > tbody > tr:nth-child(16) > td.rowfollow"),
-      // document.querySelector("#outer > table.main > tbody > tr > td > table > tbody > tr:nth-child(18) > td.rowhead.nowrap"),
-      "span",
-      {}
-    );
-    var countPTer = 0;
-    var sizePTer = 0;
-    var regex = /[+-]?\d+(\.\d+)?/g;
-    
-    for (var i = 0; i < seedList.length; i++) {
-      var seedName = seedList[i].innerText;
-      if (seedName.match(/[@-](HDS|HDTV)/i)) {
-        // var pterTag = GM_addElement(seedList[i], "a", {
-        //   class: "tag tag-jz",
-        //   style: "margin-left: 2px;",
-        //   textContent: "官种",
-        // });
-        var seedSizeStr = seedListSize[i].innerText;
-        var num = seedSizeStr.match(regex).map(function (v) {
-          return parseFloat(v);
-        });
-        var size = 0;
-        if (seedSizeStr.indexOf("KB") > 0) {
-          size = num * 1024;
-        } else if (seedSizeStr.indexOf("MB") > 0) {
-          size = num * 1024 * 1024;
-        } else if (seedSizeStr.indexOf("GB") > 0) {
-          size = num * 1024 * 1024 * 1024;
-        } else if (seedSizeStr.indexOf("TB") > 0) {
-          size = num * 1024 * 1024 * 1024 * 1024;
-        } else {
-          size = num;
-        }
-        countPTer++;
-        sizePTer += size;
+  if (bytes === 0) return "0 Bytes";
+
+  const k = 1024;
+  const dm = decimals < 0 ? 0 : decimals;
+  const sizes = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
+
+  const i = Math.floor(Math.log(bytes) / Math.log(k));
+
+  return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + " " + sizes[i];
+}
+
+function getSeedList(seedHtml) {
+  var seedList = seedHtml.querySelectorAll(
+    "table > tbody > tr > td:nth-child(2) > a"
+  );
+  var seedListSize = seedHtml.querySelectorAll(
+    "table > tbody > tr > td:nth-child(6)"
+  );
+  var summary = GM_addElement(
+    document.querySelector(
+      "table > tbody > tr > td > table > tbody > tr:nth-child(16) > td.rowfollow"
+    ),
+    // document.querySelector("#outer > table.main > tbody > tr > td > table > tbody > tr:nth-child(18) > td.rowhead.nowrap"),
+    "span",
+    {}
+  );
+  var countPTer = 0;
+  var sizePTer = 0;
+  var regex = /[+-]?\d+(\.\d+)?/g;
+
+  for (var i = 0; i < seedList.length; i++) {
+    var seedName = seedList[i].title;
+    if (seedName.match(/[@-]\s?(HDS)/i)) {
+      seedList[i].parentNode.style = "background-color: lightgreen;";
+      // var pterTag = GM_addElement(seedList[i], "a", {
+      //   class: "optiontag",
+      //   style: "color: #fff;background-color: blue;",
+      //   textContent: "官种",
+      // });
+      var seedSizeStr = seedListSize[i + 1].innerText;
+
+      var num = seedSizeStr.match(regex).map(function (v) {
+        return parseFloat(v);
+      });
+      var size = 0;
+      if (seedSizeStr.indexOf("KB") > 0) {
+        size = num * 1024;
+      } else if (seedSizeStr.indexOf("MB") > 0) {
+        size = num * 1024 * 1024;
+      } else if (seedSizeStr.indexOf("GB") > 0) {
+        size = num * 1024 * 1024 * 1024;
+      } else if (seedSizeStr.indexOf("TB") > 0) {
+        size = num * 1024 * 1024 * 1024 * 1024;
+      } else {
+        size = num;
       }
+      countPTer++;
+      sizePTer += size;
     }
-  
-    summary.innerHTML =
-      "<p>官种数量 " +
-      countPTer +
-      " 官种大小 " +
-      formatBytes(sizePTer) +
-      "<br></p>";
   }
-  
-  (function () {
-    "use strict";
-  
-    var useridStr = window.location.href.match(/\/userdetails.php\?id=(\d+)/i)[1];
+
+  summary.innerHTML =
+    "<p>官种数量 " +
+    countPTer +
+    " 官种大小 " +
+    formatBytes(sizePTer) +
+    "<br></p>" +
+    seedHtml.innerHTML;
+}
+
+(function () {
+  "use strict";
+
+  var sumOtBtn = GM_addElement(
+    document.querySelector(
+      "#outer > table > tbody > tr > td > table > tbody > tr:nth-child(16) > td.rowfollow"
+    ),
+    "button",
+    {
+      id: "sumary_ot",
+    }
+  );
+
+  sumOtBtn.innerHTML = "官种统计";
+  sumOtBtn.onclick = function () {
+    var useridStr = window.location.href.match(
+      /\/userdetails.php\?id=(\d+)/i
+    )[1];
     var urlTorrentList =
       "https://hdsky.me/getusertorrentlistajax.php?userid=" +
       useridStr +
       "&type=seeding";
-  
+
     GM.xmlHttpRequest({
       method: "GET",
       url: urlTorrentList,
@@ -90,5 +113,5 @@ function formatBytes(bytes, decimals = 2) {
         getSeedList(responseHTML);
       },
     });
-  })();
-  
+  };
+})();
