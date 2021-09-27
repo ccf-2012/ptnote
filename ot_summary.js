7a6c076ab5da2bbe2ad60618a5fa78bce4ee0425
geng <fuse66@163.com>
2021-09-27 23:56:31 +0800

    那谁，hdchina

--- ot_summary.js
+++ ot_summary.js
@@ -1,106 +1,129 @@
 // ==UserScript==
 // @name         官种保种统计
 // @namespace    https://greasyfork.org/zh-CN/scripts/432969
-// @version      0.7
-// @description  count the size of the seeding official torrents, support PTer, SKY, OB, CHD, Hares, PTH, hddolby, tjupt, TTG
+// @version      0.8
+// @description  count the size of the seeding official torrents, support PTer, SKY, OB, CHD, Hares, PTH, hddolby, tjupt, TTG, HDH, ???, HDC
 // @author       ccf2012
 // @match        https://hdsky.me/userdetails.php?id=*
 // @match        https://ourbits.club/userdetails.php?id=*
 // @match        https://chdbits.co/userdetails.php?id=*
 // @match        https://club.hares.top/userdetails.php?id=*
-// @match        https://pthome.net/userdetails.php?id=*
-// @match        https://www.hddolby.com/userdetails.php?id=*
-// @match        https://www.tjupt.org/userdetails.php?id=*
+// @match        https://*.pthome.net/userdetails.php?id=*
+// @match        https://*.hddolby.com/userdetails.php?id=*
+// @match        https://*.tjupt.org/userdetails.php?id=*
 // @match        https://totheglory.im/userdetails.php?id=*
+// @match        https://hdhome.org/userdetails.php?id=*
+// @match        https://hdchina.org/userdetails.php?id=*
+// @match        https://那谁谁.net/userdetails.php?id=*
 // @match        https://pterclub.com/userdetails.php?id=*
-// @match        https://pterclub.com/getusertorrentlist.php?userid=*&type=seeding
 // @icon         https://ourbits.club//favicon.ico
 // @grant        GM_addElement
-// @grant        GM.xmlHttpRequest
+// @grant        GM_addStyle
 // ==/UserScript==
 
 var config = {
-  "https://hdsky.me": {
-    seedList: "table > tbody > tr > td:nth-child(2) > a",
-    seedListSize: "table > tbody > tr > td:nth-child(6)",
-    seedingSummary:
-      "table > tbody > tr > td > table > tbody > tr:nth-child(16) > td.rowfollow",
+  "hdsky.me": {
+    seedList: "#ka1 > table > tbody > tr > td:nth-child(2) > a",
+    seedListSize: "#ka1 > table > tbody > tr > td:nth-child(6)",
+    seedingSummary: "#ka1 > b",
     matchRegex: /[@-]\s?(HDS)/i,
     useTitle: true,
-    ajaxGet: true,
+    isAjaxGet: true,
   },
-  "https://pterclub.com": {
-    seedList: "#outer > table > tbody > tr > td:nth-child(2) > a:nth-child(1)",
-    seedListSize: "#outer > table > tbody > tr > td:nth-child(4)",
-    seedingSummary: "#outer > h1",
+  "pterclub.com": {
+    seedList: "#ka1 > table > tbody > tr > td:nth-child(2) > a:nth-child(1)",
+    seedListSize: "#ka1 > table > tbody > tr > td:nth-child(4)",
+    seedingSummary: "#ka1",
     matchRegex: /[@-]\s?(PTer)/i,
     useTitle: true,
-    ajaxGet: false,
+    isAjaxGet: false,
   },
-  "https://ourbits.club": {
-    seedList: "table > tbody > tr > td:nth-child(2) > a",
-    seedListSize: "table > tbody > tr > td:nth-child(3)",
-    seedingSummary:
-      "table > tbody > tr > td > table > tbody > tr:nth-child(18) > td.rowfollow",
+  "ourbits.club": {
+    seedList: "#ka1 > table > tbody > tr > td:nth-child(2) > a",
+    seedListSize: "#ka1 > table > tbody > tr > td:nth-child(3)",
+    seedingSummary: "#ka1 > b",
     matchRegex: /[@-]\s?(Our|iLoveTV|FLTTH|Ao|MGs|HosT|iLoveHD)/i,
     useTitle: true,
-    ajaxGet: true,
+    isAjaxGet: true,
   },
-  "https://chdbits.co": {
-    seedList: "table > tbody > tr > td:nth-child(2) > a",
-    seedListSize: "table > tbody > tr > td:nth-child(3)",
-    seedingSummary:
-      "table > tbody > tr > td > table > tbody > tr:nth-child(16) > td.rowfollow",
+  "chdbits.co": {
+    seedList: "#ka1 >  table > tbody > tr > td:nth-child(2) > a",
+    seedListSize: "#ka1 >  table > tbody > tr > td:nth-child(3)",
+    seedingSummary: "#ka1 > b",
     matchRegex: /[@-]\s?(CHD|blucook|HQC|GBT|KAN)/i,
     useTitle: true,
-    ajaxGet: true,
+    isAjaxGet: true,
   },
-  "https://club.hares.top": {
-    seedList: "table > tbody > tr > td:nth-child(2) > a",
-    seedListSize: "table > tbody > tr > td:nth-child(3)",
-    seedingSummary:
-      "table > tbody > tr > td > table > tbody > tr:nth-child(18) > td.rowfollow",
+  "club.hares.top": {
+    seedList: "#ka1 >  table > tbody > tr > td:nth-child(2) > a",
+    seedListSize: "#ka1 >  table > tbody > tr > td:nth-child(3)",
+    seedingSummary: "#ka1 > b",
     matchRegex: /[@-]\s?(Hares)/i,
     useTitle: true,
-    ajaxGet: true,
+    isAjaxGet: true,
   },
-  "https://pthome.net": {
-    seedList: "table > tbody > tr > td:nth-child(2) > a",
-    seedListSize: "table > tbody > tr > td:nth-child(3)",
-    seedingSummary:
-      "table > tbody > tr > td > table > tbody > tr:nth-child(17) > td.rowfollow",
+  "pthome.net": {
+    seedList: "#ka1 >  table > tbody > tr > td:nth-child(2) > a",
+    seedListSize: "#ka1 >  table > tbody > tr > td:nth-child(3)",
+    seedingSummary: "#ka1 > b",
     matchRegex: /[@-]\s?(PTH)/i,
     useTitle: true,
-    ajaxGet: true,
+    isAjaxGet: true,
   },
-  "https://www.hddolby.com": {
-    seedList: "table > tbody > tr > td:nth-child(2) > a",
-    seedListSize: "table > tbody > tr > td:nth-child(3)",
-    seedingSummary:
-      "table > tbody > tr > td > table > tbody > tr:nth-child(17) > td.rowfollow",
+  "那谁谁.net": {
+    seedList: "#ka1 >  table > tbody > tr > td:nth-child(2) > a",
+    seedListSize: "#ka1 >  table > tbody > tr > td:nth-child(3)",
+    seedingSummary: "#ka1 > b",
+    matchRegex: /[@-]\s?(CMCT)/i,
+    useTitle: true,
+    isAjaxGet: true,
+  },
+  "hdhome.org": {
+    seedList: "#ka1 >  table > tbody > tr > td:nth-child(2) > a",
+    seedListSize: "#ka1 >  table > tbody > tr > td:nth-child(3)",
+    seedingSummary: "#ka1 > b",
+    matchRegex: /[@-]\s?(HDH)/i,
+    useTitle: true,
+    isAjaxGet: true,
+  },
+  "hdchina.org": {
+    seedList: "#ka1 > table > tbody > tr > td:nth-child(2) > a",
+    seedListSize: "#ka1 > table > tbody > tr > td:nth-child(3)",
+    seedingSummary: "#ka1 > p",
+    matchRegex: /[@-]\s?(HDC|k9611|tudou|iHD)/i,
+    useTitle: true,
+    isAjaxGet: true,
+  },
+  "hddolby.com": {
+    seedList: "#ka1 > table > tbody > tr > td:nth-child(2) > a",
+    seedListSize: "#ka1 > table > tbody > tr > td:nth-child(3)",
+    seedingSummary: "#ka1 > b",
     matchRegex: /[@-]\s?(Dream|DBTV|QHstudIo|HDo)/i,
     useTitle: true,
-    ajaxGet: true,
+    isAjaxGet: true,
   },
-  "https://www.tjupt.org": {
-    seedList: "table > tbody > tr > td:nth-child(2) > a",
-    seedListSize: "table > tbody > tr > td:nth-child(3)",
-    seedingSummary:
-      "table > tbody > tr > td > table > tbody > tr:nth-child(19) > td.rowfollow",
+  "tjupt.org": {
+    seedList: "#ka1 > table > tbody > tr > td:nth-child(2) > a",
+    seedListSize: "#ka1 > table > tbody > tr > td:nth-child(3)",
+    seedingSummary: "#ka1 > b",
     matchRegex: /[@-]\s?(TJUPT)/i,
     useTitle: true,
-    ajaxGet: true,
+    isAjaxGet: true,
   },
-  "https://totheglory.im": {
+  "totheglory.im": {
     seedList: "#ka2 > table > tbody > tr > td:nth-child(2) > a",
     seedListSize: "#ka2 > table > tbody > tr > td:nth-child(4)",
     seedingSummary:
       "#main_table > tbody > tr:nth-child(1) > td > table > tbody > tr > td > table > tbody > tr:nth-child(16) > td:nth-child(2)",
     matchRegex: /[@-]\s?(TTG|Wiki|NGB|DoA|ARiN|ExREN)/i,
     useTitle: false,
-    ajaxGet: false,
+    isAjaxGet: false,
   },
 };
+
+config["www.tjupt.org"] = config["tjupt.org"];
+config["www.hddolby.com"] = config["hddolby.com"];
+config["www.pthome.net"] = config["pthome.net"];
 
 function formatBytes(bytes, decimals = 2) {
   if (bytes === 0) return "0 Bytes";
@@ -116,26 +139,21 @@ function formatBytes(bytes, decimals = 2) {
 
 function getSeedList(seedHtml) {
   var seedList = seedHtml.querySelectorAll(
-    config[window.location.origin].seedList
+    config[window.location.host].seedList
   );
   var seedListSize = seedHtml.querySelectorAll(
-    config[window.location.origin].seedListSize
+    config[window.location.host].seedListSize
   );
-  var summary = GM_addElement(
-    document.querySelector(config[window.location.origin].seedingSummary),
-    "span",
-    {}
-  );
+
   var countOT = 0;
   var sizeOT = 0;
   var regex = /[+-]?\d+(\.\d+)?/g;
-
   for (var i = 0; i < seedList.length; i++) {
     var seedName;
-    if (config[window.location.origin].useTitle) seedName = seedList[i].title;
+    if (config[window.location.host].useTitle) seedName = seedList[i].title;
     else seedName = seedList[i].innerText;
 
-    if (seedName.match(config[window.location.origin].matchRegex)) {
+    if (seedName.match(config[window.location.host].matchRegex)) {
       seedList[i].parentNode.style = "background-color: lightgreen;";
       var seedSizeStr = seedListSize[i + 1].innerText;
 
@@ -159,68 +177,26 @@ function getSeedList(seedHtml) {
     }
   }
 
-  summary.innerHTML =
-    "<p>官种数量 " + countOT + " 官种大小 " + formatBytes(sizeOT) + "<br></p>";
-  if (config[window.location.origin].ajaxGet)
-    summary.innerHTML += seedHtml.innerHTML;
+  var summary = document.querySelector(config[window.location.host].seedingSummary);
+  summary.innerHTML = 
+    "<p>官种数量 " + countOT + " 官种大小 " + formatBytes(sizeOT) + "<br></p>" + summary.innerHTML ;
 }
 
 (function () {
   "use strict";
-  if (window.location.origin == "https://pterclub.com") {
-    if (window.location.href.match(/getusertorrentlist/i)) {
-      getSeedList(document);
-    } else {
-      var useridStr = window.location.href.match(
-        /\/userdetails.php\?id=(\d+)/i
-      )[1];
-      // var params = url.split("?")[1].split("&");
-      var urlPterTorrentList =
-        "https://pterclub.com/getusertorrentlist.php?userid=" +
-        useridStr +
-        "&type=seeding";
 
-      var summary = GM_addElement(
-        document.querySelector("#row_current_seeding"),
-        "button",
-        {}
-      );
-      summary.innerHTML = "<a href=" + urlPterTorrentList + ">保种统计</a>";
-    }
+  if (window.location.host == "totheglory.im") {
+    getSeedList(document);
   } else {
-    var sumOtBtn = GM_addElement(
-      document.querySelector(config[window.location.origin].seedingSummary),
-      "button",
-      {
-        id: "sumary_ot",
+    var intv = setInterval(function() {
+      var elems = document.querySelectorAll('#ka1 > table > tbody > tr');
+      if (elems && elems.length <  1){
+            return false;
       }
-    );
-    sumOtBtn.innerHTML = "官种统计";
-    var urlTorrentList;
-
-    sumOtBtn.onclick = function () {
-      if (window.location.origin == "https://totheglory.im") {
-        getSeedList(document);
-      } else {
-        var useridStr = window.location.href.match(
-          /\/userdetails.php\?id=(\d+)/i
-        )[1];
-        urlTorrentList =
-          window.location.origin +
-          "/getusertorrentlistajax.php?userid=" +
-          useridStr +
-          "&type=seeding";
-
-        GM.xmlHttpRequest({
-          method: "GET",
-          url: urlTorrentList,
-          onload: function (response) {
-            var responseHTML = document.createElement("html");
-            responseHTML.innerHTML = response.response;
-            getSeedList(responseHTML);
-          },
-        });
-      }
-    };
+      //when element is found, clear the interval.
+      clearInterval(intv);
+      // alert("loaded.");
+      getSeedList(document);
+    }, 1000);
   }
 })();
