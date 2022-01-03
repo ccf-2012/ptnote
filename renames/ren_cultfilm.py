2b7ab8e185c0e0550a90454f4fdfe4284b95fe79
ccf2012 <crlf66@163.com>
2022-01-03 16:08:37 +0800

    search link, size

--- renames/ren_cultfilm.py
+++ renames/ren_cultfilm.py
@@ -2,6 +2,7 @@ import os
 import shutil
 import argparse
 import re
+from renames.humanbytes import HumanBytes
 from tortitle import parseMovieName
 
 
@@ -10,6 +11,18 @@ def ensureDir(file_path):
         file_path = os.path.dirname(file_path)
     if not os.path.exists(file_path):
         os.makedirs(file_path)
+
+def sumSize(start_path = '.'):
+    # total_size = 0
+    # for dirpath, dirnames, filenames in os.walk(start_path):
+    #     for f in filenames:
+    #         fp = os.path.join(dirpath, f)
+    #         # skip if it is symbolic link
+    #         if not os.path.islink(fp):
+    #             total_size += os.path.getsize(fp)
+
+    total_size = sum(d.stat().st_size for d in os.scandir(start_path) if d.is_file())
+    return total_size
 
 
 def hdlinkCopy(fromLoc, toLoc):
@@ -41,7 +54,8 @@ def replaceCultFilms(fromDir):
 
             if os.path.isdir(movieFullPath):
                 parseTitle, parseYear, parseSeason, cntitle = parseMovieName(movieItem)
-                print('%s %s   %s' % (parseTitle, parseYear, mediaTargeDir))
+                sizeStr = HumanBytes(sumSize)
+                print('https://<site domain>/torrents.php?searchstr=%s+%s  ( %s ) : %s' % (parseTitle, parseYear, sizeStr, mediaTargeDir))
                 hdlinkCopy(movieFullPath, mediaTargeDir)
 
 
