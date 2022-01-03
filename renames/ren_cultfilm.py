197084a627b974752129fda074d749f94c12a192
ccf2012 <crlf66@163.com>
2022-01-03 16:36:28 +0800

    sumsize

--- renames/ren_cultfilm.py
+++ renames/ren_cultfilm.py
@@ -21,15 +21,15 @@ def ensureDir(file_path):
         os.makedirs(file_path)
 
 def sumSize(start_path = '.'):
-    # total_size = 0
-    # for dirpath, dirnames, filenames in os.walk(start_path):
-    #     for f in filenames:
-    #         fp = os.path.join(dirpath, f)
-    #         # skip if it is symbolic link
-    #         if not os.path.islink(fp):
-    #             total_size += os.path.getsize(fp)
+    total_size = 0
+    for dirpath, dirnames, filenames in os.walk(start_path):
+        for f in filenames:
+            fp = os.path.join(dirpath, f)
+            # skip if it is symbolic link
+            if not os.path.islink(fp):
+                total_size += os.path.getsize(fp)
 
-    total_size = sum(d.stat().st_size for d in os.scandir(start_path) if d.is_file())
+    # total_size = sum(d.stat().st_size for d in os.scandir(start_path) if d.is_file())
     return total_size
 
 
